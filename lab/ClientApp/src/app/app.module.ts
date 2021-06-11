395480200dbb571e01bb4c9ac48dc0b9399905c7
Sylek <sylek38@gmail.com>
2021-06-11 14:30:07 +0200

    deleted: home, counter, fetch-data components and their routes

--- lab/ClientApp/src/app/app.module.ts
+++ lab/ClientApp/src/app/app.module.ts
@@ -6,9 +6,6 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ListManagerComponent } from './list-manager/list-manager.component';
 import { SumViewComponent } from './sum-view/sum-view.component';
 
@@ -16,9 +13,6 @@ import { SumViewComponent } from './sum-view/sum-view.component';
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     ListManagerComponent,
     SumViewComponent,
   ],
@@ -27,9 +21,6 @@ import { SumViewComponent } from './sum-view/sum-view.component';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
   providers: [],
